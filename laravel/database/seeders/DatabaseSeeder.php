86d758614d2fdc4f259dde40e0fd42d2822fe0ca
Paul Paracci <baracci.paul@gmail.com>
2021-05-17 00:33:29 +0300

    Deleted random line copied into DatabaseSeeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -18,11 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
         \App\Models\Image::factory(10)->create();
-		\App\Models\Category::factory(5)->create();
-		\App\Models\Tag::factory(7)->create();
+		\App\Models\Category::factory(3)->create();
+		\App\Models\Tag::factory(5)->create();
 		\App\Models\Article::factory(15)->create();
 		\App\Models\Comment::factory(10)->create();
         \App\Models\Service::factory(10)->create();
-        //\App\Models\TimeChoice::factory(10)->create();
     }
 }
