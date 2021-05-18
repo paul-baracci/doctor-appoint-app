0a560c9199bd57efa6ef0a0265227ca64ef45752
Paul Paracci <baracci.paul@gmail.com>
2021-05-18 23:02:35 +0300

    Added email template for appointment requests

---
+++ laravel/resources/views/emails/appointment.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+    <h1>Appointment Request Form</h1>
+
+    <p>Dear Colleague,</p>
+
+    <p>An appointment has been requested. Please follow up with a confirmation</p>
+
+    <h2>Details</h2>
+
+    <p>From: {{ $name }} </p>
+    <p>Email: {{ $email }} </p>
+    <p>Phone: {{ $phone }} </p>
+    <p>Service: {{ $service }} </p>
+    <p>Time choice: {{ $options }} </p>
+
+</body>
+
+<footer>
+    <h3>Augmented Heart Logo</h3>
+</footer>
+
+</body>
+
+</html>
