46c2434f6d65b61613929e122ca9c7942bf38cd8
Paul Paracci <baracci.paul@gmail.com>
2021-07-11 14:59:23 +0300

    Added diffForHumans for article and comment timestamps

--- laravel/resources/views/blog/index.blade.php
+++ laravel/resources/views/blog/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container text">
   <h1 class="text-center">Our Blog</h1>
-  
+
   <div class="row justify-content-center">
     {{ $articles->links() }}
   </div>
@@ -12,12 +12,11 @@
             <img class="thumbnail" src="img/hero3.jpg">
             <div class="card-body">
               <h3 class="mb-1">{{ $article->article_title }}  </h3>
-              <div class="mb-1 text-muted"> <strong>Published on </strong>{{ $article->published_at }}</div>
-              <div class="mb-1 text-muted"> <strong>Author </strong>{{ $article->doctor->name }}</div>
+              <div class="mb-1 text-muted"> <strong>Published </strong>{{ $article->created_at->diffForHumans() }} by {{ $article->doctor->name }}</div>
               <p class="card-text mb-auto">{{ $article->article_description }}</p>
               <a href="/article/{{ $article->id }}">Continue reading</a>
               <p class="card-text mb-auto">category: <a href="/category/{{ $article->category_id }}"> {{ $article->category->name }} </a></p>
-              <p class="card-text mb-auto">tags: 
+              <p class="card-text mb-auto">tags:
                 @foreach($article->tags as $tag)
                   <a href="/tag/{{ $tag->id }}"> {{ $tag->name }} </a>
                 @endforeach
--- laravel/resources/views/blog/show.blade.php
+++ laravel/resources/views/blog/show.blade.php
@@ -6,7 +6,7 @@
             <div class ="col">
                 <img class="img-fluid" src="../img/hero3.jpg">
                 <h2>{{ $article->article_title }}</h2>
-                <p>Author: {{ $article->doctor->name }}, published on {{ $article->published_at }}</p>
+                <p>Author: {{ $article->doctor->name }}, published {{ $article->created_at->diffForHumans() }}</p>
                 <p>{{ $article->article_description }}</p>
                 <p class="card-text mb-auto">category: <a href="/category/{{ $article->category_id }}"> {{ $article->category->name }} </a></p>
                 <p class="card-text mb-auto">tags:
@@ -25,7 +25,7 @@
             <h3 class="m-5">Comments</h3>
             @foreach($article->comments as $comment)
                 <div class="m-5">
-                    <p>posted by {{ $comment->user->name }} on {{ $comment->approved_at }} </p>
+                    <p>posted by {{ $comment->user->name }} {{ $comment->created_at->diffForHumans() }} </p>
                     <p> {{ $comment->message }} </p>
                     <hr>
                 </div>
