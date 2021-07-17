bc12a405d5d21b395eb6a9ece6d1c33f6e1ceab7
Paul Paracci <baracci.paul@gmail.com>
2021-07-17 19:31:17 +0300

    Adjusted indentation in index.blade.php

--- laravel/resources/views/blog/index.blade.php
+++ laravel/resources/views/blog/index.blade.php
@@ -1,17 +1,17 @@
 @extends('app')
 
 @section('content')
-<div class="container text">
-  <h1 class="text-center m-4">Our Blog</h1>
-  <div class="row m-2 justify-content-center">
-    {{ $articles->links() }}
-  </div>
-  @foreach($articles as $article)
-          @include('blog.articles')
-  @endforeach
-  <div class="row mt-4 justify-content-center">
-    {{ $articles->links() }}
-  </div>
-</div>
+    <div class="container text">
+        <h1 class="text-center m-4">Our Blog</h1>
+        <div class="row m-2 justify-content-center">
+            {{ $articles->links() }}
+        </div>
+        @foreach($articles as $article)
+            @include('blog.articles')
+        @endforeach
+        <div class="row mt-4 justify-content-center">
+            {{ $articles->links() }}
+        </div>
+    </div>
 
 @endsection
